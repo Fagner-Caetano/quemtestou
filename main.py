b9aff3d75c2f4d5282a5846038f5e291566c78fa
Fagner Caetano <caetanofagner29@gmail.com>
2025-04-15 17:23:23 -0300

    Update main.py
    alteração na main, como solicitado pelo flake8

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
 # Validação se o n° é par ou impar
 
-def VerificarParImpar(numero):
-  if numero % 2 == 0:
-    return f"{numero} eh um numero par."
-  else:
-    resultado = f"{numero} eh um numero impar."
-    return resultado
 
-NumeroTeste = 10
-resultado = VerificarParImpar(NumeroTeste)
+def verificar_par_impar(numero):
+    if numero % 2 == 0:
+        return f"{numero} eh um numero par."
+    else:
+        resultado = f"{numero} eh um numero impar."
+        return resultado
+
+
+numero_teste = 10
+resultado = verificar_par_impar(numero_teste)
 print(resultado)
 
 numero_teste_2 = 7
-resultado_2 = VerificarParImpar(numero_teste_2)
+resultado_2 = verificar_par_impar(numero_teste_2)
 print(resultado_2)
-
-import os
