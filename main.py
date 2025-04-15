da5e02e5ea09792b1473a17922bcb388a9889feb
Fagner Caetano <caetanofagner29@gmail.com>
2025-04-15 14:49:53 -0300

    Correção Pylint

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 # Validação se o n° é par ou impar
 
-def verificar_par_impar(numero):
+def VerificarParImpar(numero):
   if numero % 2 == 0:
-    return f"{numero} é um número par."
+    return f"{numero} eh um numero par."
   else:
-    return f"{numero} é um número ímpar."
+    resultado = f"{numero} eh um numero impar."
+    return resultado
+
+NumeroTeste = 10
+resultado = VerificarParImpar(NumeroTeste)
+print(resultado)
+
+numero_teste_2 = 7
+resultado_2 = VerificarParImpar(numero_teste_2)
+print(resultado_2)
+
+import os
