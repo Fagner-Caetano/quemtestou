b336981723852d1962ad6a9f73e808c931540aa0
Fagner Caetano <caetanofagner29@gmail.com>
2025-04-27 20:02:13 -0300

    Alteração
    Refatoração da main.py sem mudar o comportamento.
    
    Instalação da biblioteca pytest.
    
    Criação do teste unitario.

--- main.py
+++ main.py
@@ -1,13 +1,23 @@
 import time
 import datetime
 
-count = 1
-try:
-    print("Container iniciado. Pressione Ctrl+C para encerrar.")
-    while True:
-        current_time = datetime.datetime.now().strftime("%H:%M:%S")
-        print(f"[{current_time}] O container rodou {count}x...")
-        count += 1
-        time.sleep(5)  # intervalo de 5 segundos.
-except KeyboardInterrupt:
-    print("\nContainer encerrado pelo usuário.")
+def get_current_time():
+    return datetime.datetime.now().strftime("%H:%M:%S")
+
+def print_container_message(count):
+    current_time = get_current_time()
+    print(f"[{current_time}] O container rodou {count}x...")
+
+def main_loop():
+    count = 1
+    try:
+        print("Container iniciado. Pressione Ctrl+C para encerrar.")
+        while True:
+            print_container_message(count)
+            count += 1
+            time.sleep(5)
+    except KeyboardInterrupt:
+        print("\nContainer encerrado pelo usuário.")
+
+if __name__ == "__main__":
+    main_loop()
